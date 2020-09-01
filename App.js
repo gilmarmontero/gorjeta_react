3d7bd7d88aa5dbccb06cf45c1c105e2feb796084
Lailson Henrique <lailsonh@ufpi.edu.br>
2020-09-01 15:33:04 -0300

    Alterando o botão para utilizar estilos

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React, {useState} from 'react';
 import styled from 'styled-components';
-import {Text} from 'react-native'
-
-
+import {Text} from 'react-native';
 
 const Pagina = styled.SafeAreaView`
   flex: 1;
   align-items: center;
-
 `;
 
 const Cabecalho = styled.Text`
@@ -25,13 +22,18 @@ const Input = styled.TextInput`
   padding: 12px;
 `;
 
-const Calcular = styled.Button`
-  
+const Calcular = styled.TouchableOpacity`
+  margin-top: 10px;
+  background-color: #2296f3;
+  padding: 10px 10px 10px 10px;
 `;
 
-const Resultado = styled.Button`
-  
+const TextoCalcular = styled.Text`
+  color: white;
+  font-size: 15px;
 `;
+
+const Resultado = styled.Button``;
 
 const AreaResultado = styled.View`
   width: 100%;
@@ -50,38 +52,38 @@ const TituloResultado = styled.Text`
 const ItemResult = styled.Text`
   font-size: 16px;
   margin-bottom: 30px;
-
 `;
 
 export default () => {
-
-  const [conta, alteraconta] = useState("");
+  const [conta, alteraconta] = useState('');
 
   return (
     <Pagina>
       <Cabecalho> Calculadora de gorgeta </Cabecalho>
-      <Input 
-        placeholder="Quanto deu a conta?" 
-        keyboardType="numeric" 
-        value={conta} 
-        onChangeText={(valor)=> alteraconta(valor)}
-
+      <Input
+        placeholder="Quanto deu a conta?"
+        keyboardType="numeric"
+        value={conta}
+        onChangeText={(valor) => alteraconta(valor)}
       />
-      <Calcular title="10%" />
+      <Calcular>
+        <TextoCalcular>10%</TextoCalcular>
+      </Calcular>
 
-      <Resultado title="Calcular" />
+      <Calcular>
+        <TextoCalcular>Calcular</TextoCalcular>
+      </Calcular>
 
-      {conta > 0 &&
-
-      <AreaResultado>
-        <TituloResultado>Valor da Conta</TituloResultado>
-        <ItemResult>{parseFloat(conta).toFixed(2)}</ItemResult>
-        <TituloResultado>Valor da Gorjeta</TituloResultado>
-        <ItemResult> R$ {(conta / 10).toFixed(2)}</ItemResult>
-        <TituloResultado>Valor Total</TituloResultado>
-        <ItemResult>r$ {(conta * 1.1).toFixed(2)}</ItemResult>
-      </AreaResultado>
-      }
+      {conta > 0 && (
+        <AreaResultado>
+          <TituloResultado>Valor da Conta</TituloResultado>
+          <ItemResult>{parseFloat(conta).toFixed(2)}</ItemResult>
+          <TituloResultado>Valor da Gorjeta</TituloResultado>
+          <ItemResult> R$ {(conta / 10).toFixed(2)}</ItemResult>
+          <TituloResultado>Valor Total</TituloResultado>
+          <ItemResult>r$ {(conta * 1.1).toFixed(2)}</ItemResult>
+        </AreaResultado>
+      )}
     </Pagina>
-  )
-}
+  );
+};
